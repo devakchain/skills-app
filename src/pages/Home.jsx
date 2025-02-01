1085f6ceb8c4813f89d40170f8bef49af4ad8ab3
devakchain <shmoelyitzhak@gmail.com>
2025-02-01 16:05:25 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="react same">
+        <h4>REACT-JS</h4>
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/1126/1126012.png"
+          alt="react-js"
+        />
+      </div>
+      <div className="node same">
+        <h4> NODE-JS</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=54087&format=png"
+          alt="node-js"
+        />
+      </div>
+      <div className="html same">
+        <h4> HTML</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=20909&format=png"
+          alt="html"
+        />
+      </div>
+      <div className="css same">
+        <h4> CSS</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=21278&format=png"
+          alt="css"
+        />
+      </div>
+      <div className="bootstrap same">
+        <h4> BOOTSTRAP</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=PndQWK6M1Hjo&format=png"
+          alt="bootstrap"
+        />
+      </div>
+      <div className="javascript same">
+        <h4> JAVA-SCRIPT</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=PXTY4q2Sq2lG&format=png"
+          alt="java-script"
+        />
+      </div>
+      <div className="redux same">
+        <h4> REDUX</h4>
+        <img
+          src="https://img.icons8.com/?size=160&id=3VGtaw5gCc8T&format=png"
+          alt="redux"
+        />
+      </div>
+      <div className="mongodb same">
+        <h4> MONGO-DB</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=74402&format=png"
+          alt="mongodb"
+        />
+      </div>
+      <div className="nextjs same">
+        <h4> NEXT-JS</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=MWiBjkuHeMVq&format=png"
+          alt="next-js"
+        />
+      </div>
+      <div className="mysql same">
+        <h4> MYSQL</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=UFXRpPFebwa2&format=png"
+          alt="mysql"
+        />
+      </div>
+      <div className="git same">
+        <h4> GIT</h4>
+        <img
+          src="https://img.icons8.com/?size=96&id=20906&format=png"
+          alt="git"
+        />
+      </div>
+      <div className="reactnative same">
+        <h4> REACT-NATIVE</h4>
+        <img
+          src="https://img.icons8.com/?size=128&id=p57acgidi9bj&format=png"
+          alt="react-native"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/JsReact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JsReact() {
+  return (
+    <div>
+      <h1>JsReact</h1>
+    </div>
+  );
+}
+
+export default JsReact;
